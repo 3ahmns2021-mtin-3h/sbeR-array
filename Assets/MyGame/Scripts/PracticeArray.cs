df77cd4273951ad178420711038c0a03bca5404d
Kuschlsoke <mail2bergersi@gmail.com>
2021-05-17 13:18:24 +0200

    first commit

---
+++ Assets/MyGame/Scripts/PracticeArray.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PracticeArray : MonoBehaviour
+{
+    public int[] array1 = new int[4];
+    public int[] array2 = new int[4] { 21, 22, 23, 24 };
+    public int[] array3 = { 31, 32, 33, 34, 35, 36, 37 };
+    public string[] array4 = new string[10];
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+
+        for (int i = 0; i < array4.Length; i++)
+        {
+            array4[i] = "array4";
+        }
+
+        GameObject[] arrayColoredImagesRed = GameObject.FindGameObjectsWithTag("RedImage");
+        Debug.Log(arrayColoredImagesRed);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
